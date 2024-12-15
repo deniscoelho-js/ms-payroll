94e097fd3992a1285f6748a5e72a88eecc5c1672
Denis Coelho <diego.sousa@capgemini.com>
2024-12-15 20:58:14 -0300

    implementando a interface WorkerFeignClient

--- src/main/java/core/io/hr_payroll/service/PaymentService.java
+++ src/main/java/core/io/hr_payroll/service/PaymentService.java
@@ -2,6 +2,7 @@ package core.io.hr_payroll.service;
 
 import core.io.hr_payroll.entity.Payment;
 import core.io.hr_payroll.entity.Worker;
+import core.io.hr_payroll.feignclients.WorkerFeignClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -14,16 +15,10 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class PaymentService {
 
-    @Value("${hr-worker.host}")
-    private String workerHost;
-
-    private final RestTemplate restTemplate;
+    private final WorkerFeignClient workerFeignClient;
 
     public Payment getPayment(long workerId, int days){
-        Map<String, String> uruVariables = new HashMap<>();
-        uruVariables.put("id", ""+workerId);
-
-        Worker worker = restTemplate.getForObject( workerHost + "/workers/{id}",  Worker.class, uruVariables);
+        Worker worker = workerFeignClient.findById(workerId).getBody();
         return new Payment(worker.getName(), worker.getDailyIncome(), days);
 
     }
